a0fe43b6b8181e07bbb27a30801f29ab53fa1db5
Abby Van Der Brink <145610294+AbbyjVDB@users.noreply.github.com>
2025-09-22 16:09:15 -0500

    Add files via upload

---
+++ src/main/java/com/coms309/duality/controllers/PaymentController.java
@@ -0,0 +1,39 @@
+package com.coms309.duality.controllers;
+
+import com.coms309.duality.model.Payment;
+import com.coms309.duality.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+    @Autowired
+    private PersonRepository personRepository;
+
+    /**
+     * Validates length of card details.
+     *
+     * @param cardNumber - the number of the card
+     * @param expirationDate - the cards expiration date in MM/YY format
+     * @param cvv - the cards cvv value
+     * @return true or false depending on the validity of the card
+     */
+    public boolean isCardValid(String cardNumber, String expirationDate, String cvv) {
+        if (cardNumber.matches("\\d{16}") && expirationDate.matches("(0[1-9]|1[0-2])/?[0-9]{2}") && cvv.matches("\\d{3}")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean buy(Payment payment) {
+        if (!isCardValid(payment.getCardNumber(), payment.getExpirationDate(), payment.getCvv())) {
+            return false;
+        }
+        payment.setBought(true);
+        return true;
+    }
+
+}
---
+++ src/main/java/com/coms309/duality/model/UserItems.java
@@ -0,0 +1,41 @@
+package com.coms309.duality.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="user_items")
+public class UserItems {
+    @Getter
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name="person_id")
+    private Person person;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name="item_id")
+    private Item item;
+
+    private LocalDateTime purchaseDate;
+
+    public UserItems(){}
+
+    public UserItems(Person person, Item item) {
+        this.person = person;
+        this.item = item;
+        this.purchaseDate = LocalDateTime.now();
+    }
+
+}
